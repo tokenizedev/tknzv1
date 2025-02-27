cbba9a709545acdb27460b1c2852c0eb608da79e
web-mech <webmech@gmail.com>
2025-02-26 22:00:10 -0500

    Revert "fix: Fix copying contentScript file to src/contentScript.tsx in link-build-to-source.sh"
    This reverts commit d78cc05a761814172f2cf1347e9f4ab15c8b08ec.

--- scripts/link-build-to-source.sh
+++ scripts/link-build-to-source.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Find the contentScript-[hash].js file in the dist folder
+# i need it this file: "contentScript-HpWyLiM6.js" but this may be dynamic
+content_script_file=$(ls dist/assets/contentScript-????????.js)
+
+
+# Copy the contents of the contentScript file to src/contentScript.tsx
+cat "$content_script_file" > dist/src/contentScript.tsx
+
+echo "Contents of $content_script_file copied to dist/src/contentScript.tsx"
+
 echo "Done"
