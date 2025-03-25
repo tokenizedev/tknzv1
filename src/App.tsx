b6ba5f96506885f2a31ec3e4903fda70d2b47586
web-mech <webmech@gmail.com>
2025-03-25 14:03:03 -0400

    feat: Update conditional rendering logic in App component to show VersionCheck component only if isLatestVersion is true

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ function App() {
           <WalletSetup />
         ) : showWallet ? (
           <WalletPage />
-        ) : true ? (
+        ) : isLatestVersion ? (
           <VersionCheck updateAvailable={updateAvailable || ''} />
         ) : (
           <CoinCreator />
