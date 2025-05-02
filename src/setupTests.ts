e272949f6202d07cee0499e1df809c56b58d5216
web-mech <webmech@gmail.com>
2025-05-02 00:56:08 -0400

    feat: Add fetch mock setup using vitest-fetch-mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,12 +1,17 @@
 // src/setupTests.ts
 // Global setup for Vitest + Testing Library + MSW
+import { vi } from 'vitest'
+import createFetchMock from 'vitest-fetch-mock'
 import '@testing-library/jest-dom';
 import { server } from './mocks/server';
 
+const fetchMocker = createFetchMock(vi)
+
+fetchMocker.enableMocks()
 // Establish API mocking before all tests.
 beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
 // Reset any request handlers that are declared as a part of our tests
 // so they don't affect other tests.
 afterEach(() => server.resetHandlers());
 // Clean up after the tests are finished.
-afterAll(() => server.close());
+
