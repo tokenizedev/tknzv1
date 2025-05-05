925a0fedf9aec1adf579b801985e40e3be4fc2d6
web-mech <webmech@gmail.com>
2025-05-05 02:39:06 -0400

    fix(components): fix loader display issue in CoinCreator component

--- src/components/CoinCreator.tsx
+++ src/components/CoinCreator.tsx
@@ -559,7 +559,7 @@ export const CoinCreator: React.FC<CoinCreatorProps> = ({
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [isCarouselOpen, articleData.images, carouselIndex]);
 
-  if (isLoading) {
+  if (true) {
     return <Loader isSidebar={isSidebar} />;
   }
 
