50071950f58ba6704746deccd476088725cd2a9b
web-mech <webmech@gmail.com>
2025-05-14 09:40:02 -0400

    feat(build): Copy contentScript file to .js for manifest
    - Changed the file extension from .tsx to .js for the contentScript file copied to the build directory. Updated corresponding echo message.

--- scripts/link-build-to-source.sh
+++ scripts/link-build-to-source.sh
@@ -5,9 +5,10 @@
 content_script_file=$(ls dist/assets/contentScript-????????.js)
 
 
-# Copy the contents of the contentScript file to src/contentScript.tsx
-cat "$content_script_file" > dist/src/contentScript.tsx
+# Copy the contents of the contentScript file to dist/src/contentScript.js for manifest
+cat "$content_script_file" > dist/src/contentScript.js
 
-echo "Contents of $content_script_file copied to dist/src/contentScript.tsx"
+# No longer using .tsx for content script, .js has been copied
 
+echo "Contents of $content_script_file copied to dist/src/contentScript.js"
 echo "Done"
